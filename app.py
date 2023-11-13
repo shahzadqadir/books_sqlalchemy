6e1d92dc1fe8b6d4bc4b23bb69a893544ad96396
Shahzad Qadir <shahzadqadir@hotmail.co.uk>
2023-11-13 11:27:08 +0000

    added book analysis

--- app.py
+++ app.py
@@ -166,7 +166,18 @@ def app():
                     time.sleep(1.5)
         elif choice == "4":
             # book analysis
-            pass
+            oldest_book = session.query(Book).order_by(Book.published_date).first()
+            newest_book = session.query(Book).order_by(Book.published_date.desc()).first()
+            total_books = session.query(Book).count()
+            python_books = session.query(Book).filter(Book.title.like('%Python%')).count()
+            print(f"""\n*** BOOK ANALYSIS ***
+                \rOldest Book: {oldest_book}
+                \rNewest Book: {newest_book}
+                \rNumber of Total Books: {total_books}
+                \rNumber of Python Books: {python_books}""")
+            input("\nPress enter to return to the main menu.")
+
+
         else:
             print("Good bye!")
             break
